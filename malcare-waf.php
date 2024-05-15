1e6efd6e3994695004f2469a22c73cc4324360c5
ArdataCode <ardata.indonesia@gmail.com>
2024-05-15 16:06:01 +0700

    Initial commit

---
+++ malcare-waf.php
@@ -0,0 +1,9 @@
+<?php
+// Please validate auto_prepend_file setting before removing this file
+
+if (file_exists('/home/jawa2439/public_html/cucispringbedprobolinggo.com/wp-content/plugins/malcare-security/protect/prepend/ignitor.php')) {
+	define("MCDATAPATH", '/home/jawa2439/public_html/cucispringbedprobolinggo.com/wp-content/mc_data/');
+	define("MCCONFKEY", 'ac82f20c91003a1fc4f515a5f1c8e02f');
+	include_once('/home/jawa2439/public_html/cucispringbedprobolinggo.com/wp-content/plugins/malcare-security/protect/prepend/ignitor.php');
+}
+?>
